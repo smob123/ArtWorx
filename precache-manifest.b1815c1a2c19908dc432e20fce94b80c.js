92dd81de1e9fe1de6b172a286c9d8ee723c58637
Sultan <smob123@hotmail.com>
2019-06-09 15:55:08 +1200

    Updates

---
+++ precache-manifest.b1815c1a2c19908dc432e20fce94b80c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ee6cdb8fc82f0a30a1a7d2841983dd",
+    "url": "/ArtWorx/index.html"
+  },
+  {
+    "revision": "1a616d61360b46af1117",
+    "url": "/ArtWorx/static/css/main.8290bded.chunk.css"
+  },
+  {
+    "revision": "d2862d8b1ebde24ef21e",
+    "url": "/ArtWorx/static/js/2.682622ea.chunk.js"
+  },
+  {
+    "revision": "1a616d61360b46af1117",
+    "url": "/ArtWorx/static/js/main.906c5991.chunk.js"
+  },
+  {
+    "revision": "4b03861f07d17db74bb5",
+    "url": "/ArtWorx/static/js/runtime~main.07055f4c.js"
+  },
+  {
+    "revision": "3efe212c9fef20097d964b7e575bf3aa",
+    "url": "/ArtWorx/static/media/background.3efe212c.jpg"
+  },
+  {
+    "revision": "6168061cd33b7272ef9e74075e998e6c",
+    "url": "/ArtWorx/static/media/background.6168061c.jpg"
+  },
+  {
+    "revision": "c0a30e97d026657062fb52dc84c35ea6",
+    "url": "/ArtWorx/static/media/background.c0a30e97.jpg"
+  },
+  {
+    "revision": "2191b85a8c805f894ca47b173127cfb0",
+    "url": "/ArtWorx/static/media/img1.2191b85a.png"
+  },
+  {
+    "revision": "34ebe9fd1aefe7600cc247361b9580d6",
+    "url": "/ArtWorx/static/media/img1.34ebe9fd.jpg"
+  },
+  {
+    "revision": "9a847a9ee48d0f449834995080291965",
+    "url": "/ArtWorx/static/media/img1.9a847a9e.jpg"
+  },
+  {
+    "revision": "ac4a9cc0ee804824fccb27bc5c1f1649",
+    "url": "/ArtWorx/static/media/img1.ac4a9cc0.jpg"
+  },
+  {
+    "revision": "c1cf49b280f3e62274c3bc78570b021b",
+    "url": "/ArtWorx/static/media/img1.c1cf49b2.jpg"
+  },
+  {
+    "revision": "c8d3047504f61eb810964a758280aec4",
+    "url": "/ArtWorx/static/media/img1.c8d30475.jpg"
+  },
+  {
+    "revision": "6f4aa0c0b97eead2a76407c96897c1a4",
+    "url": "/ArtWorx/static/media/img2.6f4aa0c0.jpg"
+  },
+  {
+    "revision": "99387616fc5ceb4966ea3db922b2e338",
+    "url": "/ArtWorx/static/media/img2.99387616.jpg"
+  },
+  {
+    "revision": "b3812e131f04a34438966eb4f024a8cc",
+    "url": "/ArtWorx/static/media/img2.b3812e13.jpg"
+  },
+  {
+    "revision": "b5f70cb4e95ade823d61d75ba0c44ae8",
+    "url": "/ArtWorx/static/media/img2.b5f70cb4.jpg"
+  },
+  {
+    "revision": "014197a2f4ea4a756e125bb73d212b06",
+    "url": "/ArtWorx/static/media/img3.014197a2.jpg"
+  },
+  {
+    "revision": "117a5734b328851e81c3ee8de067ab75",
+    "url": "/ArtWorx/static/media/img3.117a5734.jpg"
+  },
+  {
+    "revision": "d86c098f8c75cc1de7895c1142ba657f",
+    "url": "/ArtWorx/static/media/img3.d86c098f.jpg"
+  },
+  {
+    "revision": "ddabfcaf0f97de4360d05e2a8f5b73de",
+    "url": "/ArtWorx/static/media/img3.ddabfcaf.jpg"
+  },
+  {
+    "revision": "b9ee593d2ce3bb20f135319c34ec3bd8",
+    "url": "/ArtWorx/static/media/img4.b9ee593d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,16 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ArtWorx/precache-manifest.47acc8d0b6843615b7efb461b8557c04.js"
+  "/ArtWorx/precache-manifest.b1815c1a2c19908dc432e20fce94b80c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ArtWorx/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ArtWorx/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
